6e85b5d9fda255c8a202930ff9ec7513c7744d78
Yueying <wangyy395@gmail.com>
2020-06-22 18:17:48 -0400

    add channels

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :channels
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount ActionCable.server, at: '/cable'
 end
